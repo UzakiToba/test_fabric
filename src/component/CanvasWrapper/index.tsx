37dc3a46d5ceb3c2a441121f823159468a02014e
Uzaki Toba <yukuzu0912@gmail.com>
2020-07-18 21:11:34 +0900

    '基本操作'

--- src/component/CanvasWrapper/index.tsx
+++ src/component/CanvasWrapper/index.tsx
@@ -1,19 +1,14 @@
 import React, {useEffect, useRef} from 'react';
 import { fabric } from 'fabric';
+import {StaticCanvas} from "fabric/fabric-impl";
 
 export const CanvasWrapper: React.FC = () => {
   const canvasWrapperRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (canvasWrapperRef.current) {
-      const canvasElement = document.createElement('canvas');
-      canvasElement.id = 'test';
-      canvasWrapperRef.current.appendChild(canvasElement);
-      fabricInit();
-    }
-  }, [canvasWrapperRef]);
+  let canvas: fabric.Canvas;
+  let test: any;
 
   const fabricInit = () => {
-    const canvas = new fabric.Canvas('test');
+    canvas = new fabric.Canvas('test');
     const rect = new fabric.Rect({
       left: 100,
       top: 100,
@@ -22,7 +17,53 @@ export const CanvasWrapper: React.FC = () => {
       height: 20
     });
     canvas.add(rect);
+    console.log({ canvas });
   }
 
-  return <div ref={canvasWrapperRef}>test</div>;
+  useEffect(() => {
+    if (canvasWrapperRef.current) {
+      const canvasElement = document.createElement('canvas');
+      canvasElement.id = 'test';
+      canvasWrapperRef.current.appendChild(canvasElement);
+      fabricInit();
+    }
+  }, [canvasWrapperRef, fabricInit]);
+
+  const check = () => {
+    test = canvas.toJSON();
+    console.log(canvas.toJSON());
+  };
+
+  const add = () => {
+    canvas.loadFromJSON(test, () => { console.log('load') });
+  }
+
+  const add2 = () => {
+    const rect = new fabric.Rect({
+      left: 50,
+      top: 100,
+      fill: 'green',
+      width: 80,
+      height: 20
+    });
+    canvas.add(rect)
+  }
+
+  const update = () => {
+    // 選択する
+    const a = canvas.item(0);
+    if (a instanceof fabric.Object)
+      canvas.setActiveObject(a);
+    // 選択中のobject
+    const hoge = canvas.getActiveObject();
+    hoge.set('fill', 'blue');
+    canvas.renderAll();
+  }
+
+  return <div id="hoge" ref={canvasWrapperRef}>
+    <button onClick={check}>check</button>
+    <button onClick={add}>add</button>
+    <button onClick={add2}>add2</button>
+    <button onClick={update}>update</button>
+  </div>;
 };
